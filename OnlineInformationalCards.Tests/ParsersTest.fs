c893ac346b5b19aa6b756c1c8f845b8217b669a0
Martin Grotz <martin.grotz@gmx.de>
2020-09-14 17:48:00 +0200

    Add tests for JSON parsers

--- OnlineInformationalCards.Tests/ParsersTest.fs
+++ OnlineInformationalCards.Tests/ParsersTest.fs
@@ -1,10 +1,11 @@
 module ParsersTest
 
+open System
 open Expecto
+open Swensen.Unquote
+open Thoth.Json.Net
 open Types
 open Parsers
-open Swensen.Unquote
-open Fleece.Newtonsoft
 
 [<Tests>]
 let tests =
@@ -12,9 +13,56 @@ let tests =
         "DeckMetadata"
         [ testCase "should error on invalid id field"
           <| fun () ->
-              let j =
+              let actual =
                   """{"id": 3, "title": "X", "description": "abc"}"""
+                  |> Decode.fromString DeckMetadata.Decoder
 
-              let x: DeckMetadata ParseResult = parseJson j
+              test
+                  <@ Expect.wantError actual "Err expected" = "Error at: `$.id`\nExpecting a guid but instead got: 3" @>
 
-              test <@ false = true @> ]
+          testCase "description is optional"
+          <| fun () ->
+              let actual =
+                  """{"id": "840701c0-132f-49e8-a268-9d9aecc28736", "title": "X", "description": null}"""
+                  |> Decode.fromString DeckMetadata.Decoder
+
+              test
+                  <@ Expect.wantOk actual "Ok expected" =
+                      { Id = Guid.Parse("840701c0-132f-49e8-a268-9d9aecc28736")
+                        Title = "X"
+                        Description = None } @>
+
+          testCase "title and description get parsed correctly"
+          <| fun () ->
+              let actual =
+                  """{"id": "840701c0-132f-49e8-a268-9d9aecc28736", "title": "X", "description": "Y"}"""
+                  |> Decode.fromString DeckMetadata.Decoder
+
+              test
+                  <@ Expect.wantOk actual "Ok expected" =
+                      { Id = Guid.Parse("840701c0-132f-49e8-a268-9d9aecc28736")
+                        Title = "X"
+                        Description = Some "Y" } @> ]
+
+
+[<Tests>]
+let cardTests =
+    testList
+        "Card"
+        [ testCase "errors on invalid data"
+          <| fun () ->
+              let actual =
+                  """{"id": "3b", "text": "X"}"""
+                  |> Decode.fromString Card.Decoder
+
+              test
+                  <@ Expect.wantError actual "Err expected" =
+                      "Error at: `$.id`\nExpecting an int but instead got: \"3b\"" @>
+
+          testCase "values get decoded correcly"
+          <| fun () ->
+              let actual =
+                  """{"id": 3, "text": "X"}"""
+                  |> Decode.fromString Card.Decoder
+
+              test <@ Expect.wantOk actual "Ok expected" = { Id = 3; Text = "X" } @> ]
--- OnlineInformationalCards/Parsers.fs
+++ OnlineInformationalCards/Parsers.fs
@@ -1,49 +1,17 @@
 module Parsers
 
-open Fleece.Newtonsoft
-open Fleece.Newtonsoft.Operators
+open Thoth.Json.Net
 open Types
-open AppErrors
-open FSharpPlus
 
 type DeckMetadata with
-
-    static member Create id title description =
-        { DeckMetadata.Id = id
-          Title = title
-          Description = description }
-
-    static member OfJson json =
-        match json with
-        | JObject o ->
-            DeckMetadata.Create
-            <!> (o .@ "id")
-            <*> (o .@ "title")
-            <*> (o .@ "description")
-        | x -> Decode.Fail.objExpected x
+    static member Decoder: Decoder<DeckMetadata> =
+        Decode.map3 (fun id title description ->
+            { Id = id
+              Title = title
+              Description = description }: DeckMetadata) (Decode.field "id" Decode.guid)
+            (Decode.field "title" Decode.string) (Decode.field "description" (Decode.option Decode.string))
 
 type Card with
-    static member OfJson json =
-        match json with
-        | JObject o ->
-            let id = o .@ "id"
-            let text = o .@ "text"
-            match id, text with
-            | Decode.Success id, Decode.Success text -> Decode.Success { Card.Id = id; Text = text }
-            | x ->
-                Error
-                <| ParserError(sprintf "Error parsing Card: %A" x)
-        | x ->
-            Error
-            <| ParserError(sprintf "Error parsing Card - object expected: %A" x)
-
-
-type Person = { Name: string; Age: int }
-
-type Person with
-    static member Create name age dob gender children = { Person.Name = name; Age = age }
-
-    static member OfJson json =
-        match json with
-        | JObject o -> Person.Create <!> (o .@ "name") <*> (o .@ "age")
-        | x -> Decode.Fail.objExpected x
+    static member Decoder: Decoder<Card> =
+        Decode.map2 (fun id text -> { Id = id; Text = text }: Card) (Decode.field "id" Decode.int)
+            (Decode.field "text" Decode.string)
